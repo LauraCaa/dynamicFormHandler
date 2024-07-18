ee46b17049174f222ae5f28a24d4845485e0c0d3
Camila Cardenas Castro <camila@192.168.80.19>
2024-07-17 21:58:31 -0500

    feat: New sign up form

---
+++ src/pages/create_account.js
@@ -0,0 +1,11 @@
+import "@/assets/create_account.css";
+import Form from "@/components/form2/form";
+export default function CreateAccount() {
+    return(
+        <main>
+            <div className="column">
+                <Form></Form>
+            </div>
+        </main>
+    );
+}
